9303b936b4d9b03f65d005cdaca46a3ada9dbba7
LeaB2 <133144330+LeaB2@users.noreply.github.com>
2023-05-10 18:41:49 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,6 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
-let trackIndex = 0;
-
-const goToNextTrack = () => {
-  trackIndex += 1;
-}
 
 <audio src={trackUrls[trackIndex]} autoPlay controls />
 <button onClick={goToNextTrack}>
